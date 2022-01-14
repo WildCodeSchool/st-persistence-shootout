e5a15900d4d9b02f33bf3579ef798d3fd7ab63ab
Alexander C <alexander.culum@gmail.com>
2022-01-14 12:21:42 +0100

    Initial Commit of Spring Boot Starter Project.

---
+++ src/main/java/dev/wcs/tutoring/persistenceshootout/PersistenceShootoutApplication.java
@@ -0,0 +1,13 @@
+package dev.wcs.tutoring.persistenceshootout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistenceShootoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistenceShootoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/wcs/tutoring/persistenceshootout/PersistenceShootoutApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wcs.tutoring.persistenceshootout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistenceShootoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
